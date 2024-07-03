372d9fb3c99428f398f14bc403f6b0cbcd873c2d
Habeeb <ademolawaliyu1@gmail.com>
2024-07-03 20:54:41 +0100

    commit

--- client/src/pages/Home1.jsx
+++ client/src/pages/Home1.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import Explore from '../components/explore/Explore';
-// import InspectedRoom from "../components/inspectedRoom/InspectedRoom";
-// import Offers from "../components/offers/Offers";
-// import TrandingDestination from '../components/trandingDestination/TrandingDestination';
-// import DiscoverCity from "../components/discover/DiscoverCity";
-// import HotelCategories from "../components/hotelCategories/HotelCategories";
-// import Hotels from "../components/hotels/Hotels";
-// import HotelDeals from "../components/hotelDeals/HotelDeals";
-// import Footer from "../components/footer/Footer";
+import Explore from '../components/explore/Explore';
+import InspectedRoom from "../components/inspectedRoom/InspectedRoom";
+import Offers from "../components/offers/Offers";
+import TrandingDestination from '../components/trandingDestination/TrandingDestination';
+import DiscoverCity from "../components/discover/DiscoverCity";
+import HotelCategories from "../components/hotelCategories/HotelCategories";
+import Hotels from "../components/hotels/Hotels";
+import HotelDeals from "../components/hotelDeals/HotelDeals";
+import Footer from "../components/footer/Footer";
 
 
 const Home1 = () => {
